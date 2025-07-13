c8afba9f762608476afe31c25b15a7f797ee6b28
Hsr19 <harshvardhanrathore3000@gmail.com>
2025-07-13 21:59:24 +0530

    minor change

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 const filterOption = document.querySelector(".filter-todo");
 
-//Event Listeners
+//Event Listener
 // document.addEventListener("DOMContentLoaded", getTodos);
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteTodo);
